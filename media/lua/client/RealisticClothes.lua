7a370bf91a1ce7a9e2b3c6547bbf77348a1a95cc
sonbnt <sonbnt@vng.com.vn>
2025-06-09 17:49:41 +0700

    fix logic tight clothes

--- media/lua/client/RealisticClothes.lua
+++ media/lua/client/RealisticClothes.lua
@@ -129,13 +129,13 @@ function RealisticClothes.onUpdatePlayer(player)
         elseif player:isSprinting() then
             stiffnessMul = 3.0
         end
-        local movingStiffness = 0.00075 * stiffnessMul * (math.abs(diff) + 1) / 2 * RealisticClothes.IncreaseStiffnessMultiplier
 
         local bodyPartTypes = {BodyPartType.UpperLeg_L, BodyPartType.UpperLeg_R, BodyPartType.LowerLeg_L, BodyPartType.LowerLeg_R}
         for _, bodyPartType in ipairs(bodyPartTypes) do
-            local bodyPart = player:getBodyDamamge():getBodyPart(bodyPartType)
+            local bodyPart = player:getBodyDamage():getBodyPart(bodyPartType)
             local diff = RealisticClothes.getDiffForBodyPart(bodyPartType)
             if diff < 0 then
+                local movingStiffness = 0.00075 * stiffnessMul * (math.abs(diff) + 1) / 2 * RealisticClothes.IncreaseStiffnessMultiplier
                 bodyPart:setStiffness(bodyPart:getStiffness() + movingStiffness * getGameTime():getMultiplier())
             end
         end
